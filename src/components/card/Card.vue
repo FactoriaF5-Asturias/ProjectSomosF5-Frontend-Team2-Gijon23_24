d28a4f5d945d25ba0303044da876956935af664f
Stella Maris González Robert <stegonyrob@yahoo.es>
2024-03-11 15:36:11 +0100

    card

--- src/components/card/Card.vue
+++ src/components/card/Card.vue
@@ -31,6 +31,21 @@
             </v-card-actions>
         </v-card>
     </div>
+    <template>
+  <v-card
+    class="mx-auto"
+    height="200"
+    image="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+    max-width="200"
+    theme="dark"
+    title="Card title"
+  ></v-card>
+</template>
+
+
+
+
+
 </template>
 
 
