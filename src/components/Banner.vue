ddc1ca1713a0e29c7a31d2dafa5f62394f45cd90
Nicolás Albarenque Malnero <nxcxdev@gmail.com>
2024-03-11 14:06:40 +0100

    Create banner component HTML basic structure.

---
+++ src/components/Banner.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+	<section>
+		<div class="logo-container">
+			<img />
+		</div>
+		<h1>PrintGo</h1>
+		<h2>Haciendo tangible lo inimaginable.</h2>
+		<div class="searchbar">
+			<form>
+				<input type="text" />
+				<input type="submit" />
+			</form>
+		</div>
+	</section>
+</template>
+
+<style lang="scss" scoped></style>
