39c517b61b614643c9df19f2cba1043d2cac52c4
Nicolás Albarenque Malnero <nxcxdev@gmail.com>
2024-03-11 16:31:19 +0100

    Added Banner component to HomeView & some syntax tweaks.

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -3,17 +3,58 @@
 <template>
 	<section>
 		<div class="logo-container">
-			<img />
+			<img src="/images/banner-logo.svg" />
 		</div>
-		<h1>PrintGo</h1>
+		<h1>
+			<span class="title-white">Print</span>
+			<span class="title-purple">Go</span>
+		</h1>
 		<h2>Haciendo tangible lo inimaginable.</h2>
-		<div class="searchbar">
-			<form>
+		<form>
+			<div class="searchbar">
 				<input type="text" />
 				<input type="submit" />
-			</form>
-		</div>
+			</div>
+		</form>
 	</section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h1 {
+	font-size: 4rem;
+	font-weight: 700;
+	.title-white {
+		color: white;
+	}
+	.title-purple {
+		color: #d094ff;
+	}
+}
+h2 {
+	font-size: 2rem;
+	font-weight: 500;
+	color: white;
+}
+section {
+	gap: 1rem;
+	font-family: "Poppins", sans-serif;
+	background-image: url(/images/banner_image.jpg);
+	background-size: auto 100%;
+	height: 50%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+
+.logo-container {
+	height: 30rem;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	img {
+		height: 100%;
+		object-fit: contain;
+	}
+}
+</style>
