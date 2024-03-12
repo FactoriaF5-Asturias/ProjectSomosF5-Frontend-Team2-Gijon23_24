bd31b059b1290302669b8fdb04a3e4c28ca22768
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-03-12 14:20:03 +0100

    created trash and pencil icons

--- src/components/admin-dashboard-components/DeleteAndEditButtons.vue
+++ src/components/admin-dashboard-components/DeleteAndEditButtons.vue
@@ -25,19 +25,37 @@ const editArticle = async (id) => {
 </script>  -->
 
 <template>
-    <button class="actionBtn"  @click="editArticle(article.id)">Editar</button>
-    <button class="actionBtn"  @click="deleteArticle(article.id)">Borrar</button>
-</template>
+    <div>
+       <button @click="editArticle">
+         <img  src="/icons/penNormal.svg" />
+       </button>
+       <button @click="deleteArticle">
+         <img src="/icons/trashRegular.svg" /> 
+       </button>
+    </div>
+   </template>
 
 <style lang="scss" scoped>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
-.actionBtn {
-    background-color: #AE81D1;
-    font-family: "Poppins", sans-serif;
-    color: white;
-    margin: 5px;
-    border-radius: 2px;
+div {
+    display: flex;
+    gap:1rem;
 }
+
+img {
+    width: 2.5rem;
+    height: 2.5rem;
+
+}
+
+// .actionBtn {
+//     background-color: #AE81D1;
+//     font-family: "Poppins", sans-serif;
+//     color: white;
+//     margin: 5px;
+//     border-radius: 2px;
+//     padding: 1rem 2rem;
+// }
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,13 @@
 <script setup>
 import DeleteAndEditButtons from '@/components/admin-dashboard-components/DeleteAndEditButtons.vue';
-
+import AddButton from '@/components/admin-dashboard-components/AddButton.vue';
 </script>
 
 <template>
   <main>
     <h1>This is home page</h1>
     <DeleteAndEditButtons />
+    <AddButton />
   </main>
 </template>
 
