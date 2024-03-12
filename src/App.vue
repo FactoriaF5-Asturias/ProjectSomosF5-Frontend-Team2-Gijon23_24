91c83539e15d1684fc7350db921d06290d3cc06f
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-03-12 15:27:47 +0100

    added styles and function to delete and edit button

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+
+
+
 </script>
 
 <template>
--- src/components/admin-dashboard-components/DeleteAndEditButtons.vue
+++ src/components/admin-dashboard-components/DeleteAndEditButtons.vue
@@ -1,12 +1,18 @@
-<!-- 
 <script setup>
 import { useArticleStore } from "@/stores/article";
-import { useRouter } from 'vue-router'
+import { useRouter } from 'vue-router';
+import { defineEmits } from 'vue';
 
 const router = useRouter()
 const store = useArticleStore()
 
 store.getArticles()
+
+const emit = defineEmits(['open-form']);
+
+const editArticle = () => {
+ emit('open-form');
+};
 
 const deleteArticle = async (id) => {
     const isDeleted = await store.deleteArticle(id)
@@ -16,27 +22,23 @@ const deleteArticle = async (id) => {
     }
 
 }
-
-const editArticle = async (id) => {
-    router.push('/edit-article')
-
-  
-}
-</script>  -->
+</script> 
 
 <template>
-    <div>
-       <button @click="editArticle">
-         <img  src="/icons/penNormal.svg" />
-       </button>
-       <button @click="deleteArticle">
-         <img src="/icons/trashRegular.svg" /> 
-       </button>
-    </div>
-   </template>
+  <div>
+
+    <button @click="editArticle">
+      <img  src="/icons/penNormal.svg" />
+    </button>
+
+    <button @click="deleteArticle">
+      <img src="/icons/trashRegular.svg" /> 
+    </button>
+
+  </div>
+</template>
 
 <style lang="scss" scoped>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
 div {
     display: flex;
@@ -46,16 +48,6 @@ div {
 img {
     width: 2.5rem;
     height: 2.5rem;
-
 }
-
-// .actionBtn {
-//     background-color: #AE81D1;
-//     font-family: "Poppins", sans-serif;
-//     color: white;
-//     margin: 5px;
-//     border-radius: 2px;
-//     padding: 1rem 2rem;
-// }
 
 </style>
