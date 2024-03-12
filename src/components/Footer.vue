bdb01b1daf05e25aa1b8550919e3a10948b4340d
Natalia GF <natimetal83@gmail.com>
2024-03-12 15:16:24 +0100

    fixed footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,15 +9,18 @@
         <router-link to="/faq">Preguntas Frecuentes</router-link>
         <router-link to="/about">Sobre Nosotros</router-link>
         <router-link to="/contact">Contacto</router-link>
+        
       </nav>
+      <div class="copi">
+        ©  copiright  todos los derechos reservados  
+      </div>
     </div>
     <div class="footer-right">
       <img src="../assets/images/paypal.png" alt="PayPal" class="paypal-logo" />
-      <div class="visa">
         <img src="../assets/images/visa.png" alt="visa" class="visa-logo">
       </div>
-    </div>
     
+   
   </footer>
 </template>
 
@@ -31,7 +34,7 @@
 }
 
 .logo {
-  width: 75px; 
+  width: 85px; 
 }
 
 .footer-left,
@@ -39,19 +42,22 @@
 .footer-right {
   display: flex;
   align-items: center;
-  }
+  
+}
+
 
 nav {
   display: flex;
   gap: 60px;
 }
-nav a{
+
+nav a {
   color: aliceblue;
-  text-decoration:none;
-  font-size:medium; 
-  
+  text-decoration: none;
+  font-size:large;
 }
-nav a:hover{
+
+nav a:hover {
   color: blueviolet;
 }
 
@@ -59,17 +65,20 @@ nav a:hover{
   width: 30px;
   margin-right: 10%;
 }
-.visa-logo{
+
+.visa-logo {
   width: 30px;
   margin-left: 20%;
 }
-
-
-
-
-
+.copi{
+  margin-top: 15%;
+  position: relative;
+  color: aliceblue;
+  
+}
 
 
 </style>
+
 
   
