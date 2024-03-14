26ddbe5d7157fdb3cecb0118bba5b7df4d8de2b0
Colunga-D <xkould@gmail.com>
2024-03-14 11:27:05 +0100

    edit vitest.config

--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite';
-import Vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [Vue()],
-});
+ plugins: [vue()],
+ test: {
+    globals: true,
+ }
+})
