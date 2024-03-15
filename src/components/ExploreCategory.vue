97ebfeef1d6335efce6e076f52849d53e86177a6
katherine Gómez <kaligocas1990@gmail.com>
2024-03-15 12:57:43 +0100

    finished Explore Category

---
+++ src/components/ExploreCategory.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+    
+  <div class="galeria">
+    <h1 class="text-center">Explora nuestras categorías</h1>
+    <span class="linea"></span>
+    <div class="contenedor_imagenes">
+        <div class="imagen">
+            <img src="../assets/img/litofanias.png" alt="">
+            <h2>Litofanias</h2>
+            <div class="overlay"><h2>Litofanias</h2></div>
+        </div>
+        <div class="imagen">
+            <img src="../assets/img/hogar.png" alt="">
+            <h2>Hogar</h2>
+            <div class="overlay"><h2>Hogar</h2></div>
+        </div>
+        <div class="imagen">
+            <img src="../assets/img/geek.png" alt="">
+            <h2>Geek</h2>
+            <div class="overlay"><h2>Geek</h2></div>
+        </div>
+    </div>
+  </div>  
+  
+</template>
+
+<style lang="scss" scoped>
+    @import '../assets/scss/Explore_categort.scss';
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
+  import ExploreCategory from '../components/ExploreCategory.vue';
 </script>
 
 <template>
   <main>
     <h1>This is home page</h1>
+  <ExploreCategory/>
   </main>
 </template>
 
