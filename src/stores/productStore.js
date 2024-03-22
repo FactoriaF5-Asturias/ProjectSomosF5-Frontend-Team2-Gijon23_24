ee2129b78f63a0b53aac1dd6cf3baf6bc9575447
Nicolás Albarenque Malnero <nxcxdev@gmail.com>
2024-03-22 12:35:41 +0100

    Fixed conflicts.

--- src/stores/productStore.js
+++ src/stores/productStore.js
@@ -1,26 +1,23 @@
+import { defineStore } from "pinia";
+import ProductsService from "../services/ProductsService";
 
-import { defineStore } from 'pinia';
-import ProductsService from '../services/ProductsService';
+export const useProductsStore = defineStore("products", {
+	state: () => ({
+		products: [],
+		isLoaded: true,
+	}),
 
-export const useProductsStore = defineStore('products', {
-
- state: () => ({
-    products: [],
-    isLoaded: true,
- }),
-
- actions: {
-    async fetchProducts() {
-      const productsService = new ProductsService();
-      try {
-        const products = await productsService.getProducts();
-        this.products = products;
-        this.isLoaded = true;
-      } catch (error) {
-        console.error('Error al cargar los productos:', error);
-        this.isLoaded = false; 
-      }
-    },
-
- },
+	actions: {
+		async fetchProducts() {
+			const productsService = new ProductsService();
+			try {
+				const products = await productsService.getProducts();
+				this.products = products;
+				this.isLoaded = true;
+			} catch (error) {
+				console.error("Error al cargar los productos:", error);
+				this.isLoaded = false;
+			}
+		},
+	},
 });
