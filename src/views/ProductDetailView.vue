c44af4df083f07baf8679f65d3b7e82bebf7b5e3
Natt83~ <natimetal83@gmail.com>
2024-03-22 17:12:57 +0100

    datail page

--- src/views/ProductDetailView.vue
+++ src/views/ProductDetailView.vue
@@ -1,94 +1,29 @@
+<script setup></script>
 <template>
-    <div class="product-detail">
-      <div class="product-info">
-        <div class="product-image">
-          <img :src="selectedImage" alt="Product Image">
+    <div id="home-detail">
+        <button class="back-button" onclick="goBack()"><span class="arrow-left"></span></button>
+        <div class="detail-image-container">
+            <div class="detail-mainImage-container">
+                <img src="/public/images/estatua.png" alt="">
+            </div>
+            <div class="detail-miniPics-container">
+                <img src="/public/images/estatua.png" alt="">
+                <img src="/public/images/Logo3d.png" alt="Tanjiro's plush height">
+                <img src="/public/images/PrintGo.svg" alt="kid having fun with Tanjiro's plush">
+            </div>
         </div>
-        <div class="product-minipics">
-          <img v-for="(image, index) in images" :key="index" :src="image" @mouseover="changeImage(image)">
-        </div>
-        <div class="product-description">
-          <h2>{{ product.name }}</h2>
-          <p>{{ product.details }}</p>
-          <p>Price: {{ product.price }}</p>
+
+        <div class="detail-text-container">
+            <h3>Nombre Producto<br></h3>
+            <h2>14,99€<br></h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur earum necessitatibus reprehenderit quidem esse est, veniam temporibus placeat sunt officiis. Alias nihil placeat excepturi, quisquam earum maxime eos harum inventore.</p>
         </div>
       </div>
-    </div>
   </template>
   
-  <script setup>
-  const product = {
-    name: 'Product Name',
-    details: 'Product Details Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    price: '$20'
-  };
-  
-  const images = ['/public/images/estatua.png', '/public/images/banner-logo.svg', '/public/images/Logo3d.png'];
-  let selectedImage = images[0];
-  
-  const changeImage = (image) => {
-    selectedImage = image;
-    
-  };
-  </script>
-  
-  <style scoped lang="scss">
-  .product-detail {
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
-    background-image: url('https://ih1.redbubble.net/cover.4092629.2400x600.jpg');
-    background-repeat: no-repeat;
-    background-size: cover;
-    padding-top: 50px; 
-  }
-  
-  .product-info {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    width: 60%; 
-    margin-top: 50px; 
-  }
-  
-  .product-image img {
-    width: 100%; 
-    border-radius: 5px;
-    box-shadow: 6px 8px 10px 2px #0a142b;
-  }
-  
-  .product-minipics {
-    display: flex;
-    justify-content: center;
-    margin-top: 15px;
-    
-  }
-  
-  .product-minipics img {
-    width: 60px; 
-    margin: 0 10px; 
-    cursor: pointer; 
-    border-radius: 5px;
-    box-shadow: 6px 8px 10px 2px #0a142b;
-  }
-  
-  .product-description {
-    margin-top: 5%; 
-    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-    color: black;
-    border-bottom: 2px solid;
-    margin-bottom: 5%;
 
-   
-  }
   
-  .product-description h2 {
-    font-size: 30px;
-  }
-  
-  .product-description p {
-    font-size: 20px; 
-  }
+  <style lang="scss" scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+  @import '/src/assets/scss/ProductDetail.scss';
   </style>
-  
