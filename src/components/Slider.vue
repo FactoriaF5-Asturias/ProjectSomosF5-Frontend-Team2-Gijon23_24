aefeead0814b829464b4d5ed8ed6ad6b43300698
Dani Gallo <danigallo@Dani.local>
2024-03-26 14:22:55 +0100

    slider located

---
+++ src/components/Slider.vue
@@ -0,0 +1,57 @@
+ <script>
+export default {
+ data() {
+    return {
+      items: [
+      {
+          title: 'Card 2',
+          description: 'This is the description for card 2.',
+          image: '../assets/img/Kaido.png',
+        },
+        {
+          title: 'Card 2',
+          description: 'This is the description for card 2.',
+          image: 'https://cdn.vuetifyjs.com/images/cards/forest.jpg',
+        },
+        {
+          title: 'Card 1',
+          description: 'This is the description for card 1.',
+          image: 'https://cdn.vuetifyjs.com/images/cards/desert.jpg',
+        },
+        {
+          title: 'Card 2',
+          description: 'This is the description for card 2.',
+          image: 'https://cdn.vuetifyjs.com/images/cards/forest.jpg',
+        },
+        {
+          title: 'Card 1',
+          description: 'This is the description for card 1.',
+          image: 'https://cdn.vuetifyjs.com/images/cards/desert.jpg',
+        },
+        // Agrega más objetos para más tarjetas
+      ],
+    };
+ },
+};
+</script>
+<template>
+    <v-slide-group show-arrows class="slideGroup">
+       <v-slide-group-item class="slideCard" v-for="(item, index) in items" :key="index">
+         <v-card class="mx-6" max-width="500">
+           <v-img :src="item.image" aspect-ratio="1.9"></v-img>
+           <v-card-title>{{ item.title }}</v-card-title>
+           <v-card-subtitle>{{ item.description }}</v-card-subtitle>
+         </v-card>
+       </v-slide-group-item>
+    </v-slide-group>
+   </template>
+   <style scoped>
+   .slideGroup {
+    padding: 5em;
+    align-self: center;
+    background-color: rgba(33, 33, 33, 1);
+   }
+   .slideCard{
+    background-image: url(../assets/img/Kaido.png);
+   }
+   </style> 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,13 @@
 import ExplorerCategory from '../components/ExploreCategory.vue';
 import Banner from "@/components/Banner.vue";
 import Card from '@/components/card/Card.vue';
+import Slider from '@/components/Slider.vue';
 </script>
 
 <template>
 	<main>
 		<Banner  />
-
+		<Slider />
 		<Card/>
   <ExplorerCategory/>
 
