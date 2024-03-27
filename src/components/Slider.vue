b164840e4306d0aa02464a495882e80817877724
Dani Gallo <danigallo@Dani.local>
2024-03-27 22:02:35 +0100

    slider added

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,57 +1,46 @@
- <script>
-export default {
- data() {
-    return {
-      items: [
-      {
-          title: 'Card 2',
-          description: 'This is the description for card 2.',
-          image: '../assets/img/Kaido.png',
-        },
-        {
-          title: 'Card 2',
-          description: 'This is the description for card 2.',
-          image: 'https://cdn.vuetifyjs.com/images/cards/forest.jpg',
-        },
-        {
-          title: 'Card 1',
-          description: 'This is the description for card 1.',
-          image: 'https://cdn.vuetifyjs.com/images/cards/desert.jpg',
-        },
-        {
-          title: 'Card 2',
-          description: 'This is the description for card 2.',
-          image: 'https://cdn.vuetifyjs.com/images/cards/forest.jpg',
-        },
-        {
-          title: 'Card 1',
-          description: 'This is the description for card 1.',
-          image: 'https://cdn.vuetifyjs.com/images/cards/desert.jpg',
-        },
-        // Agrega más objetos para más tarjetas
-      ],
-    };
- },
-};
+ <script setup>
+import Card from './card/Card.vue';
+import { ref } from 'vue';
+import {useProductsStore} from "./../stores/productStore";
+import CarouselCard from './card/CarouselCard.vue';
+
+
+const productStore = useProductsStore();
+const products = ref([]) 
+const getProducts = async ()=> {
+    try {
+    await productStore.fetchProducts();
+    products.value = productStore.products;
+} catch (error) {
+    console.error("No se consiguieron los datos:",error)
+}}
+getProducts();
 </script>
 <template>
     <v-slide-group show-arrows class="slideGroup">
-       <v-slide-group-item class="slideCard" v-for="(item, index) in items" :key="index">
-         <v-card class="mx-6" max-width="500">
-           <v-img :src="item.image" aspect-ratio="1.9"></v-img>
-           <v-card-title>{{ item.title }}</v-card-title>
-           <v-card-subtitle>{{ item.description }}</v-card-subtitle>
-         </v-card>
+       <v-slide-group-item class="slideCard" v-for="product in productStore.products" :key="product.id">
+        <!-- <v-card
+          :class="['ma-4', selectedClass]"
+          color="grey-lighten-1"
+          height="200"
+          width="200"
+          @click="toggle"
+        >
+    </v-card> -->
+    <CarouselCard id="card" :product="product"></CarouselCard>
+        
+         
        </v-slide-group-item>
     </v-slide-group>
    </template>
-   <style scoped>
+   <style lang="scss" scoped>
    .slideGroup {
-    padding: 5em;
+    
     align-self: center;
     background-color: rgba(33, 33, 33, 1);
    }
-   .slideCard{
-    background-image: url(../assets/img/Kaido.png);
+   #card{
+    filter: drop-shadow(1px 14px 9px #000000);;
    }
+  
    </style> 
