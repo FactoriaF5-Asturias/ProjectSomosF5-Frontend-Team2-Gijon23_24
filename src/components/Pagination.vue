d73d39d9f77ee96ba8da3debf26e4d285961cde7
Stella Maris González Robert <stegonyrob@yahoo.es>
2024-04-04 14:34:48 +0200

    fixed pagination icons

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,21 +1,16 @@
 <template>
    <div class="text-center">
-     <v-pagination
-       v-model="page"
-       :length="4"
-       next-icon="../../public/icons/chevron-right.svg"
-       prev-icon="../../public/icons/chevron-left.svg"
-     ></v-pagination>
+    <v-pagination class="pagination" :length="4"></v-pagination>
    </div>
  </template>
  
  <script>
-   export default {
-     data() {
-       return {
-         page: 1,
-       }
-     },
-   }
- </script>
  
+ </script>
+ <style lang="scss" scoped>
+.pagination{
+  margin-top: 2rem;
+}
+
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,13 @@
-import './assets/scss/main.scss'
-
+import '@mdi/font/css/materialdesignicons.css'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
-
-import App from './App.vue'
-import router from './router'
-
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import 'vuetify/styles'
-
+import App from './App.vue'
+import './assets/scss/main.scss'
+import router from './router'
 const vuetify = createVuetify({
     components,
     directives,
