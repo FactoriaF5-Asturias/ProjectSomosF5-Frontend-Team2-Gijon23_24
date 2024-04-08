099876ea9440ab0fc4fb26bb3544a0941ae6e176
Natalia GF <natimetal83@gmail.com>
2024-04-08 11:21:48 +0200

    add minipic and fixed product description

---
+++ src/components/profiles/GoBackButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <button class="goback" @click="goBack"></button>
+  </template>
+  
+  <script >
+export default {
+  methods: {
+    goBack() {
+      window.history.length > 1 ? history.go(-1) : this.$router.push('/');
+    }
+  }
+};
+  </script>
+  
+  <style scoped>
+  .goback {
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' class='icon icon-tabler icon-tabler-arrow-back-up-double' width='44' height='44' viewBox='0 0 24 24' stroke-width='1.5' stroke='%23a905b6' fill='none' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpath stroke='none' d='M0 0h24v24H0z' fill='none'/%3E%3Cpath d='M13 14l-4 -4l4 -4' /%3E%3Cpath d='M8 14l-4 -4l4 -4' /%3E%3Cpath d='M9 10h7a4 4 0 1 1 0 8h-1' /%3E%3C/svg%3E");
+    width: 65px;
+    height: 45px;
+    border: none;
+    cursor: pointer;
+    margin-left: 20px;
+    background-size: cover;
+  }
+  </style>
+  
--- src/views/ProductDetailView.vue
+++ src/views/ProductDetailView.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="home-detail" class="product-detail">
-    <button class="goback" @click="goBack"></button>
+    <GoBackButton />
+    
     <div class="detail-image-container">
       <div class="detail-mainImage-container">
         <img :src="imageDirectory" alt="Main image" />
       </div>
       <div class="detail-miniPics-container">
-        <img v-for="(image, index) in product.additionalImages" :key="index" :src="image" alt="imagenes adicionales" />
-      </div>
+      <img :src="imageDirectory" alt="Miniatura" @click="changeMainImage(imageDirectory)" :class="{ 'active': imageDirectory === selectedThumbnail }" />
+      <img v-for="(image, index) in product.additionalImages" :key="index" :src="image" alt="Miniatura" @click="changeMainImage(image)" :class="{ 'active': image === selectedThumbnail }" />
+    </div>
+
     </div>
     <div class="detail-text-container">
       <h3 class="product-name">
@@ -17,7 +20,7 @@
         {{ product.price }}<span style="font-size: 3rem">€</span>
       </h2>
       <p>
-        {{ product.description }}
+        {{ product.productDescription}}
       </p>
     </div>
     <div class="añadirCarrito-container">
@@ -35,8 +38,10 @@
 <script setup>
 import axios from "axios";
 import { onMounted, reactive, ref } from 'vue';
-import { useRoute } from 'vue-router';
+import { useRoute,useRouter } from 'vue-router';
+import GoBackButton from '../components/profiles/GoBackButton.vue';
 const route = useRoute();
+const router = useRouter();
 let product = reactive({
   productName: '',
   price: '',
@@ -45,10 +50,13 @@ let product = reactive({
   additionalImages: []
 });
 const cantidad = ref(1);
+let selectedThumbnail = '';
 onMounted(async () => {
   const id = route.params.id_product;
   const response = await axios.get(`http://localhost:8080/api/v1/products/${id}`);
   product = response.data;
+  selectedThumbnail = product.additionalImages[2];
+  imageDirectory.value = uri + "/" + findImageForProduct(product);
 });
 function sumarCantidad() {
   cantidad.value++;
@@ -58,22 +66,25 @@ function restarCantidad() {
     cantidad.value--;
   }
 }
-function goBack() {
-  window.history.length > 1 ? history.go(-1) : router.push('/');
-}
 const uri = import.meta.env.VITE_API_ENDPOINT_IMAGES;
 const imageDirectory = ref('');
 const defaultImage = '../../../public/images/banner-logo.svg';
 const isLoading = ref(true);
 function findImageForProduct(product) {
-   const image = product.images.find(img => img.mainImage === true);
-   return image.imageName ? image.imageName : defaultImage;
+  const image = product.images.find(img => img.mainImage === true);
+  return image.imageName ? image.imageName : defaultImage;
 };
+
 onMounted(async () => {
-   await new Promise(resolve => setTimeout(resolve, 2000));
-   imageDirectory.value = uri + "/" + findImageForProduct(product);
-   isLoading.value = false;
+  await new Promise(resolve => setTimeout(resolve, 2000));
+  imageDirectory.value = uri + "/" + findImageForProduct(product);
+  isLoading.value = false;
 });
+function changeMainImage(image) {
+  imageDirectory.value = uri + "/" + image;
+  selectedThumbnail = image;
+}
+
 </script>
 <style lang="scss" scoped>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
