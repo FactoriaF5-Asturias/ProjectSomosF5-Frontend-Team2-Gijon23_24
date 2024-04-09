d6b5449bb880742dafa90ee3abcc1840eb2a559f
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-04-09 15:04:13 +0200

    fixed Factoria Alert

--- src/components/alerts/FactoriaAlert.vue
+++ src/components/alerts/FactoriaAlert.vue
@@ -5,14 +5,14 @@
     <article>
         <img src="/images/factoria5-logo.png" alt="">
         <div>
-            <p>"Este sitio es solo para fines educativos. No ingrese información personal o realice pagos aquí"</p>
+            <p>"Este sitio es solo para fines educativos. No ingrese información personal o realice pagos aquí."</p>
         </div>
     </article>
 </template>
 
 <style scoped lang="scss">
 article {
-    height: 3rem;
+    height: 15rem;
     z-index: 99;
     background-color: #FF4700;
     display: flex;
@@ -20,12 +20,12 @@ article {
     align-items: center;
 
     img {
-        height: 100%;
+        height: 80%;
     }
 
     p {
         margin-top: 0.5rem;
-        font-size: 1.2rem;
+        font-size: 2.5rem;
         color: white;
     }
 }
