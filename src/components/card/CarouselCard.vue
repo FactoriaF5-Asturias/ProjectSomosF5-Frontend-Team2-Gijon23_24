701d58ab704e54555344ff0e658d927a151dbae0
Romina Ruiz <ruizdiazsanchezromina@gmail.com>
2024-04-18 10:20:39 +0200

    fixed carousel card

--- src/components/card/CarouselCard.vue
+++ src/components/card/CarouselCard.vue
@@ -12,15 +12,18 @@ const props = defineProps({
 })
 
 const uri = import.meta.env.VITE_API_ENDPOINT_IMAGES;
+
 const imageDirectory = uri + "/" + findImageForProduct(props.product);
 
-   
+
 function findImageForProduct(product) {
    const image = product.images.find(img => img.mainImage === true);
+   if (image == undefined) {
+      const defaultImage = 'placeholder-image.jpg';
+      return defaultImage;
+   }
       return image.imageName
-      
 }
-
 
 </script>
 <template>
--- src/components/general/header/Navbar.vue
+++ src/components/general/header/Navbar.vue
@@ -3,9 +3,6 @@ import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
 const navbarVisible = ref(true);
-const litofaniaProducts = ref([]);
-const houseProducts = ref([]);
-const geekProducts = ref([]);
 
 let lastScrollTop = window.scrollY || document.documentElement.scrollTop;
 
@@ -21,29 +18,6 @@ window.addEventListener('scroll', () => {
     lastScrollTop = currentScrollTop;
 });
 
-async function fetchProducts(category) {
-    try {
-        const response = await axios.get(`http://localhost:8080/api/v1/products/getManyByCategoryName?categoryName=${category}`);
-        
-        if (category === 'litophane') {
-            litofaniaProducts.value = response.data;
-        } else if (category === 'house') {
-            houseProducts.value = response.data;
-        } else if (category === 'geek') {
-            geekProducts.value = response.data;
-        }
-        
-        console.log(response.data);
-    } catch (error) {
-        console.error('Error al obtener productos:', error);
-        // Puedes agregar aquí el manejo de errores, como mostrar un mensaje al usuario
-    }
-}
-
-// Llamar a la función para obtener productos cuando el componente se monta
-// onMounted(() => {
-//     fetchProducts('litophane'); // Llama a la función para cargar productos de la categoría inicial al cargar la página
-// });
 </script>
 
 <template>
@@ -53,26 +27,6 @@ async function fetchProducts(category) {
         <router-link to="/house" exact-active-class="active" class="link">Hogar</router-link>
         <router-link to="/geek" exact-active-class="active" class="link">Geek</router-link>
     </nav>
-    <main>
-        <div v-if="litofaniaProducts.length">
-            <h2>Litofanía</h2>
-            <ul>
-                <li v-for="product in litofaniaProducts" :key="product.id">{{ product.name }}</li>
-            </ul>
-        </div>
-        <div v-if="houseProducts.length">
-            <h2>Hogar</h2>
-            <ul>
-                <li v-for="product in houseProducts" :key="product.id">{{ product.name }}</li>
-            </ul>
-        </div>
-        <div v-if="geekProducts.length">
-            <h2>Geek</h2>
-            <ul>
-                <li v-for="product in geekProducts" :key="product.id">{{ product.name }}</li>
-            </ul>
-        </div>
-    </main>
 </template>
 
 <style scoped lang="scss">
