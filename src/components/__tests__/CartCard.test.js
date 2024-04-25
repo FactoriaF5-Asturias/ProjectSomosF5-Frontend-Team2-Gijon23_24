3225bd3f9a3e32ca964032efdfce1f601e6121a0
Colunga-D <xkould@gmail.com>
2024-04-25 14:56:18 +0200

    changes on test of components, 14 OK

---
+++ src/components/__tests__/CartCard.test.js
@@ -0,0 +1,26 @@
+import { mount } from '@vue/test-utils';
+import CartCard from '../payments/stripe/CartCard.vue';
+
+test('renders the product name, description, price, and count correctly', async () => {
+  const product = {
+    productName: 'Product Name',
+    productDescription: 'Product Description',
+    price: 10.99,
+  };
+  const count = 3;
+
+  const wrapper = mount(CartCard, {
+    props: { product, count },
+  });
+
+  expect(wrapper.text()).toContain(product.productName);
+
+ 
+  expect(wrapper.text()).toContain(product.productDescription);
+
+
+  expect(wrapper.text()).toContain(`${product.price} €`);
+
+  
+  expect(wrapper.text()).toContain(`x ${count}`);
+});
