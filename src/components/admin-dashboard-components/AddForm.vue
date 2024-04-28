b68a6004e9687b24130c204ef4f659f2a75c13ad
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-04-28 16:56:05 +0200

    styles adjusted

--- src/components/admin-dashboard-components/AddForm.vue
+++ src/components/admin-dashboard-components/AddForm.vue
@@ -133,102 +133,69 @@ async function deleteProduct(productId) {
 
 <template>
 	<div class="modal" @click="closeForm">
-		<div class="modal_container" @click.stop>
-			<div id="button_container">
-				<button @click.stop="closeForm">
-					<img src="/icons/icon-cross.svg" alt="cross icon" />
-				</button>
-			</div>
-
-			<form @submit.prevent="submitForm">
-				<h1>Añadir nuevo producto</h1>
-				<div class="divMain">
-					<div class="image-main">
-						<label>Imagen Principal</label>
-						<input
-							type="file"
-							class="form-control-file"
-							id="file"
-							name="file"
-							@change="handleMainImageChange"
-						/>
-					</div>
-					<section>
-						<div class="first-row">
-							<div class="title-container">
-								<label>Título</label>
-								<input
-									type="text"
-									class="form-control"
-									id="title"
-									v-model="productName"
-									placeholder="Título"
-								/>
-							</div>
-
-							<div class="price-container">
-								<label>Precio</label>
-								<input
-									type="number"
-									class="form-control"
-									id="price"
-									v-model="price"
-									placeholder="Precio"
-								/>
-							</div>
-						</div>
-
-						<div class="categories-container">
-							<label>Categoría</label>
-							<select
-								id="categories"
-								v-model="categoryId"
-								placeholder="Seleccione categoría"
-							>
-								<option value="1">Hogar</option>
-								<option value="2">Geek</option>
-								<option value="3">Litofanía</option>
-							</select>
-						</div>
-					</section>
-				</div>
-
-				<div class="images-container">
-					<label for="file-upload" class="custom-file-upload"
-						>Imágenes</label
-					>
-					<input
-						type="file"
-						class="form-control-file"
-						id="file-upload"
-						name="files"
-						multiple
-						@change="handleFilesChange"
-					/>
-				</div>
-
-				<div class="description-container">
-					<label>Descripción</label>
-					<textarea
-						class="form-control"
-						id="description"
-						rows="3"
-						v-model="productDescription"
-						placeholder="Descripción..."
-					></textarea>
-				</div>
-
-				<div class="btns-actions">
-					<button id="reset" @click="resetForm()">Borrar</button>
-					<button id="send" @click="handlePost()">Enviar</button>
-				</div>
-			</form>
+	  <div class="modal_container" @click.stop>
+		<div id="button_container">
+		  <button @click.stop="closeForm">
+			<img src="/icons/icon-cross2.svg" alt="cross icon" />
+		  </button>
 		</div>
+		<form @submit.prevent="submitForm">
+		  <h1>Añadir nuevo producto</h1>
+		  <div class="divMain">
+			<div class="image-main">
+			  <label for="file-upload" class="custom-file-upload">
+				Imagen Principal
+			  </label>
+			  <div class="input-wrapper-1">
+			  <input type="file" class="form-control-file" id="file" name="file" @change="handleMainImageChange" />
+			  <span class="plus-symbol">+</span>
+			</div>
+			</div>
+			<section>
+			  <div class="first-row">
+				<div class="title-container">
+				  <label>Título</label>
+				  <input type="text" class="form-control" id="title" v-model="productName" placeholder="Título" />
+				</div>
+				<div class="price-container">
+				  <label>Precio</label>
+				  <input type="number" class="form-control" id="price" v-model="price" placeholder="Precio" />
+				</div>
+			  </div>
+			  <div class="categories-container">
+				<label>Categoría</label>
+				<select id="categories" v-model="categoryId" placeholder="Seleccione categoría">
+				  <option value="1">Hogar</option>
+				  <option value="2">Geek</option>
+				  <option value="3">Litofanía</option>
+				</select>
+			  </div>
+			</section>
+		  </div>
+		  <div class="images-container">
+			<label for="file-upload" class="custom-file-upload">
+			  Galería de imágenes
+			</label>
+			<div class="input-wrapper">
+			  <input type="file" class="form-control-file" id="file-upload" name="selectedFiles" multiple @change="handleFilesChange" />
+			  <span class="add-images">+ Añadir imágenes</span>
+			</div>
+		  </div>
+		  <div class="description-container">
+			<label>Descripción</label>
+			<textarea class="form-control" id="description" rows="3" v-model="productDescription"
+			  placeholder="Descripción..."></textarea>
+		  </div>
+		  <div class="btns-actions">
+			<button id="reset" @click="resetForm()">Borrar</button>
+			<button id="send" @click="handlePost()">Enviar</button>
+		  </div>
+		</form>
+	  </div>
 	</div>
-</template>
-
-<style lang="scss" scoped>
-.modal {
+  </template>
+  <style lang="scss" scoped>
+  .modal {
 	display: flex;
 	justify-content: center;
 	align-items: center;
@@ -239,164 +206,201 @@ async function deleteProduct(productId) {
 	width: 100vw;
 	background-color: rgba(0, 0, 0, 0.5);
 	z-index: 1000;
-}
-
-.modal_container {
-	height: 60rem;
+  }
+  .modal_container {
+	height: 63rem;
 	width: 60rem;
 	background-color: white;
 	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 	border-radius: 1rem;
-	border-color: #ae81d1;
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
-}
-
-#button_container {
+	border: 6px solid $primary-color;
+  }
+  img {
+	height: 3rem;
+	width: 3rem;
+  }
+  #button_container {
 	width: 100%;
 	display: flex;
-	padding: 3rem;
+	padding-right: 1rem;
+	margin-bottom: 6rem;
 	justify-content: end;
-}
-
-form {
+  }
+  form {
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
 	width: 100%;
 	max-width: 600px;
 	margin: 0 auto;
+	margin-bottom: 3rem;
 	padding: 0 2rem 0 2rem;
 	border-radius: 1rem;
-}
-
-.divMain {
+  }
+  .divMain {
 	display: flex;
 	justify-content: space-between;
 	gap: 1rem;
-}
-
-.image-main {
+  }
+  .image-main {
 	width: 30%;
-
-	input {
-		//border: 1px solid black;
-		width: 100%;
-		height: 8.7rem;
-		border-radius: 0.5rem;
-		background-color: #ddd8d8;
+	.input-wrapper-1 {
+	  width: 100%;
+	  height: 9.2rem;
+	  background-color: white;
+	  border: 2px solid #422359;
+	  border-radius: 0.5rem;
+	  position: relative;
 	}
-}
-section {
+	.input-wrapper-1 input {
+	  position: relative;
+	  z-index: 2;
+	  opacity: 0;
+	  width: 100%;
+	  height: 100%;
+	  cursor: pointer;
+	}
+	.plus-symbol {
+	  position: absolute;
+	  top: 50%;
+	  left: 50%;
+	  transform: translate(-50%, -50%);
+	  font-size: 3rem;
+	  font-weight: bolder;
+	  color: rgb(54, 54, 54);
+	}
+  }
+  section {
 	width: 100%;
-}
-
-.first-row {
+  }
+  .first-row {
 	display: flex;
 	gap: 2rem;
 	width: 100%;
 	margin-bottom: 1rem;
-}
-
-.title-container {
+  }
+  .title-container {
 	width: 70%;
-
 	input {
-		width: 100%;
-		height: 3rem;
-		border-radius: 0.5rem;
-		background-color: #ddd8d8;
-		font-size: 1rem;
+	  width: 100%;
+	  height: 3rem;
+	  border-radius: 0.5rem;
+	  background-color: white;
+	  border: 2px solid #422359;
+	  font-size: 1rem;
+	  font-weight: 300;
+	  color: rgb(54, 54, 54);
 	}
-}
-
-.price-container {
+  }
+  .price-container {
 	width: 30%;
-
 	input {
-		width: 100%;
-		height: 3rem;
-		border-radius: 0.5rem;
-		font-size: 1rem;
-		background-color: #ddd8d8;
+	  width: 100%;
+	  height: 3rem;
+	  border-radius: 0.5rem;
+	  font-size: 1rem;
+	  color: rgb(54, 54, 54);
+	  font-weight: 300;
+	  background-color: white;
+	  border: 2px solid #422359;
 	}
-}
-
-.categories-container {
+  }
+  .categories-container {
 	display: flex;
 	flex-direction: column;
 	width: 100%;
 	margin-bottom: 1rem;
-}
-
-select {
+  }
+  select {
 	width: 100%;
 	height: 3rem;
 	border-radius: 0.5rem;
-	//border: 1px solid black;
-	background-color: #ddd8d8;
-}
-
-.images-container {
-	margin-bottom: 1rem;
-
-	input {
-		border: 1px solid black;
-		width: 100%;
-		height: 5rem;
-		//border-radius: 0.5rem;
-		background-color: #ddd8d8;
+	background-color: white;
+	border: 2px solid #422359;
+  }
+   .images-container {
+	 display: flex;
+	 flex-direction: column;
+	 width: 100%;
+	 margin-bottom: 1rem;
+   }
+	.input-wrapper {
+	  position: relative;
+	  width: 100%;
+	  height: 5rem;
+	  background-color: white;
+	  border: 2px solid #422359 ;
+	  border-radius: 0.5rem;
 	}
-}
-
-.description-container {
+	.input-wrapper input {
+	  position: relative;
+	  z-index: 2;
+	  opacity: 0;
+	  width: 100%;
+	  height: 100%;
+	  cursor: pointer;
+	}
+	.add-images {
+	  position: absolute;
+	  top: 50%;
+	  left: 50%;
+	  transform: translate(-50%, -50%);
+	  font-size: 1.8rem;
+	  color: rgb(54, 54, 54);
+	}
+  .description-container {
 	width: 100%;
 	height: 15rem;
-
 	textarea {
-		width: 100%;
-		height: 12rem;
-		border-radius: 0.5rem;
-		//border: 1px solid black;
-		background-color: #ddd8d8;
-		font-size: 1rem;
+	  width: 100%;
+	  height: 12rem;
+	  border-radius: 0.5rem;
+	  background-color: white;
+	  border: 2px solid #422359;
+	  font-size: 1rem;
+	  color: rgb(54, 54, 54);
+	  font-weight: 300;
 	}
-}
-
-h1 {
+  }
+  h1 {
 	font-size: 4rem;
-	color: grey;
-	margin-bottom: 5rem;
+	font-weight: 500;
+	color: #422359;
+	margin-bottom: 4rem;
 	display: flex;
 	justify-content: center;
-}
-
-label {
-	font-size: 1.5rem;
-	margin-bottom: 1rem;
-}
-
-.btns-actions {
-	display: flex;
-	justify-content: center;
-	gap: 2rem;
+  }
+  label {
+	font-size: 1.3rem;
+	color: #422359;
+	font-weight: 400;
 	margin-top: 1rem;
-
+  }
+  .btns-actions {
+	display: flex;
+	justify-content: center;
+	gap: 6rem;
+	margin-top: 2rem;
 	button {
-		background-color: #ae81d1;
-		font-size: 1.5rem;
-		color: white;
-		border-radius: 0.5rem;
-
-		width: 8rem;
-		height: 4rem;
-		display: flex;
-		justify-content: center;
-		align-items: center;
+	  background-color: $primary-color;
+	  font-size: 1.5rem;
+	  color: white;
+	  border-radius: 0.5rem;
+	  width: 8rem;
+	  height: 4rem;
+	  display: flex;
+	  justify-content: center;
+	  align-items: center;
+	  transition: ease-in-out 2s;
 	}
-}
-.form-control {
+	 button:hover {
+	  background-color: #422359;
+	}
+  }
+  .form-control {
 	padding: 1rem;
-}
-</style>
+  }
+  </style>
--- src/components/profiles/user/UserProfile.vue
+++ src/components/profiles/user/UserProfile.vue
@@ -116,67 +116,49 @@ const cancelData = () => {
 </script>
 
 <template>
-
   <body>
-
       <div class="user-profile">
         <h1>PERFIL DE USUARIO</h1>
       </div>
-
       <div class=" modal-container">
-
         <div class="logo-container">
           <img class="image-logo" src="/images/logo.svg" alt="logo-profile">
         </div>
-
         <form @submit.prevent="submitForm">
-
           <div class="input-box">
             <label>Nombre</label>
-            <input type="text" id="firstName" v-model="firstName"> 
+            <input type="text" id="firstName" v-model="firstName">
           </div>
-
           <div class="input-box">
             <label>Apellidos</label>
             <input type="text" id="lastName" v-model="lastName">
           </div>
-
           <div class="input-box">
             <label>Teléfono</label>
             <input type="text" id="numberPhone" v-model="numberPhone">
           </div>
-
           <div class="input-box">
             <label>Dirección</label>
             <input type="text" id="address" v-model="address">
           </div>
-
           <div class="input-box-2">
             <label>C. P.</label>
             <input type="text" id="postal-code" v-model="postalCode">
-
             <label>Ciudad</label>
             <input type="text" id="city" v-model="city">
           </div>
-
           <div class="input-box">
             <label>Provincia</label>
             <input type="text" id="province" v-model="province">
           </div>
-
           <div class="btns-container">
             <button id="cancel" @click="cancelData()">Cancelar</button>
             <button id="save" @click="saveData">Guardar</button>
           </div>
-
         </form>
-
       </div>
     </body>
-    
-  
 </template>
-
 <style lang="scss" scoped>
 body {
   background-color: #CBABE4;
@@ -185,74 +167,63 @@ body {
   align-items: center;
   flex-direction: column;
 }
-
 .modal-container {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 20rem;
-  height: 85rem;
+  padding: 4rem;
+  height: 70rem;
+  width: 60;
   border-radius: 1.5rem;
   box-shadow: 0 0 30px rgba(0, 0, 0, 0.8);
   background-color: white;
   position: sticky;
   max-height: 90rem;
   margin-bottom: 10rem;
-  
 }
-@media (max-width: 916px) {
+@media (max-width: 1000px) {
   .modal-container{
     width: 80%;
-    
   }
 }
-
 .user-profile {
   display: flex;
   justify-content: center;
-  margin: 20rem 0 5rem 0;
+  margin: 5rem 0 5rem 0;
 }
-
 h1 {
   font-weight: 600;
   text-align: center;
-  font-size: 5rem;
+  font-size: 4rem;
   color: $primary-background;
   font-family: "Poppins", sans-serif;
 }
-
 .logo-container {
   display: flex;
   justify-content: center;
   flex-direction: column;
   align-items: center;
-  margin-top: 2rem;
+  margin-top: 1rem;
 }
-
 .image-logo {
   height: 13rem;
 }
-
 form {
   display: flex;
   flex-direction: column;
   align-items: center;
   font-family: "Poppins", sans-serif;
   font-size: 2rem;
-
 }
-
 .input-box {
   display: flex;
   flex-direction: column;
-
   label {
     font-size: 1.8rem;
     margin-top: 1rem;
     margin-bottom: 0.5rem;
   }
-
   input {
     width: 40rem;
     padding: 0.5rem 1rem;
@@ -261,7 +232,6 @@ form {
     border-radius: 0.5rem;
   }
 }
-
 @media (max-width: 539px) {
   .input-box{
     input{
@@ -273,30 +243,23 @@ form {
   display: flex;
   flex-direction: row;
 }
-
 .input-box-2 {
-
   display: flex;
   flex-direction: row;
   gap: 0.5rem;
   margin-top: 2rem;
-
   label {
     font-size: 2rem;
     margin-top: 1rem;
     margin-right: 1rem;
-
   }
-
   #postal-code {
     width: 10rem;
     padding: 0.5rem 1rem;
     font-size: 1.8rem;
     border: 1px solid gray;
     border-radius: 0.5rem;
-
   }
-
   #city {
     width: 15rem;
     padding: 0.5rem 1rem;
@@ -304,33 +267,27 @@ form {
     border: 1px solid gray;
     border-radius: 0.5rem;
   }
-
   @media (max-width: 517px){
     #postal-code, #city{
       width: 100%;
     }
   }
 }
-
 @media (max-width: 517px) {
   .input-box-2{
     flex-direction: column;
-    
   }
 }
-
 .btns-container {
   margin-top: 2rem;
   display: flex;
   gap: 4rem;
-
   button {
     background-color: $primary-color;
     font-family: "Poppins", sans-serif;
     font-size: 1.8rem;
     color: white;
     border-radius: 0.5rem;
-
     width: 12rem;
     height: 4rem;
     display: flex;
@@ -339,11 +296,9 @@ form {
     cursor: pointer;
     transition: transform 0.2s ease;
   }
-
   button:hover {
     background-color: #3C3057;
   }
-
   button:active {
     transform: scale(1.1);
   }
